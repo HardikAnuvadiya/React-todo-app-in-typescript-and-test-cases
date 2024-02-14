81fc7801ea25739d75506651531ef5cd33a9da94
HardikAnuvadiya <hardik.anuvadiya@gmail.com>
2024-02-14 18:29:41 +0530

    working

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Todo } from '../Todo/Todo';
-// import { useDispatch } from 'react-redux';
-// import { logOut } from '../../store/user/action';
 import { persistor } from '../../store';
 
 export const HomePage: React.FC = () => {
-  // const dispatch: any = useDispatch();
   return (
     <div>
       <button
         onClick={async () => {
-          // dispatch(logOut());
           return await persistor.purge();
         }}
       >
--- src/store/index.ts
+++ src/store/index.ts
@@ -6,15 +6,25 @@ import loggingMiddleware from '../middleware/loggingMiddleware';
 import { userReducer } from './user/reducer';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/es/storage';
+// import { encryptTransform } from 'redux-persist-transform-encrypt';
 
 const rootReducer: any = combineReducers({
   todoState: todoReducer,
   userState: userReducer,
 });
 
-const persistConfig = {
+// const secretKey = 'sdmksd@$324iru2#@#';
+// const encryptionTransform = encryptTransform({
+//   secretKey: secretKey,
+//   onError: function () {
+//     // Handle the error.
+//   },
+// });
+
+const persistConfig: any = {
   key: 'root',
   storage,
+  // transforms: [encryptionTransform], // Apply the transform
   // Optionally, you can blacklist certain reducers or whitelist only specific ones.
   // blacklist: ['userState'],
   whitelist: ['todoState', 'userState'],
